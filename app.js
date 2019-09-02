5a5faff5302d9762678b780e9e8f9de957e98fa0
Marco Bolognesi <marco.bolognesi.73@gmail.com>
2019-09-02 15:26:40 +0100

    update server connection

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, () => {
-// app.listen(process.env.PORT, process.env.IP, function(){
+// app.listen(3000, () => {
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server started");
 });
