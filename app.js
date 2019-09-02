55d526a7311a1fd0919aa7427a2902e5415114d7
Marco Bolognesi <marco.bolognesi.73@gmail.com>
2019-09-02 15:09:29 +0100

    add file campgrounds_url.text

--- app.js
+++ app.js
@@ -57,6 +57,7 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
+// app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, () => {
     console.log("Server started");
 });
