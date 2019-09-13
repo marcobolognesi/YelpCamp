30f25f4db2af5732f2287ec34b350eadc656692f
Marco Bolognesi <marco.bolognesi.73@gmail.com>
2019-09-13 17:36:58 +0100

    correct spelling mistake in alert message

--- middleware/index.js
+++ middleware/index.js
@@ -49,7 +49,7 @@ middlewareObj.isLoggedIn = function(req, res, next) {
     if(req.isAuthenticated()) {
         return next();
     }
-    req.flash("error", "You need to be looged in to do that!");
+    req.flash("error", "You need to be logged in to do that!");
     res.redirect("back");
 };
 
